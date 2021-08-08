0d68e8d22adcd19048768b2c3d1653a4703f39c1
Hawkus <77986031+HawkUs-Git@users.noreply.github.com>
2021-08-08 17:14:37 +0000

    Hello123

--- main.py
+++ main.py
@@ -166,8 +166,9 @@ def sources_no_arg():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
+
